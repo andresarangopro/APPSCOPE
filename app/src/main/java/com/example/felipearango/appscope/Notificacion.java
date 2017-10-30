fe2ebfb48a4a9994c970728f201de470fffd5a29
Sebassllr <sebaslunasllr@gmail.com>
2017-10-29 20:00:50 -0500

    Cambio al Graddle agregando el Recicler view

---
+++ app/src/main/java/com/example/felipearango/appscope/Notificacion.java
@@ -0,0 +1,41 @@
+package com.example.felipearango.appscope;
+
+/**
+ * Created by Sebastian Luna R on 10/29/2017.
+ */
+
+public class Notificacion {
+
+    ////////////////////////////////
+    ////////Variables
+    /////////////////////////////////
+    private final String nombreTrabajo;
+    private final String nombreEmpresa;
+    private final String estado;
+
+    /////////////////////////////////
+    /////Constructor
+    //////////////////////////////////
+
+    public Notificacion(String nombreTrabajo, String nombreEmpresa, String estado) {
+        this.nombreTrabajo = nombreTrabajo;
+        this.nombreEmpresa = nombreEmpresa;
+        this.estado = estado;
+    }
+
+    //////////////////////////////////
+    /////////Getters
+    //////////////////////////////////
+
+    public String getNombreTrabajo() {
+        return nombreTrabajo;
+    }
+
+    public String getNombreEmpresa() {
+        return nombreEmpresa;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+}
